6296f047911249f7f02743ff8acb1e89104db114
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2021-03-17 22:53:57 -0600

    fix

--- backend/src/routes/api/products.ts
+++ backend/src/routes/api/products.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Router } from 'express';
+import * as Logger from '../../utils/logger';
 import multer from 'multer';
 import path from 'path';
 import {IResponse} from '../../utils/types';
