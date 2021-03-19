485fa6ab32b67c3b328bc662dfc61e1a7ebdcb70
Aarón Reynoza <ldaaron38@gmail.com>
2021-03-18 17:51:04 -0700

    fix angular compiler and search endpoint (#26)
    * fix angular compiler and search endpoint
    
    * more fixes

--- backend/src/routes/api/search.ts
+++ backend/src/routes/api/search.ts
@@ -1,73 +1,83 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Router } from 'express';
 import * as Logger from '../../utils/logger';
-import {Filter} from '../../utils/types';
+import { Filter } from '../../utils/types';
+
 const log = Logger.getInstance();
 
 // eslint-disable-next-line import/prefer-default-export
 export const handler = (router: Router, routesContext: any) => {
-
   router.post('/search', async (req, res) => {
     const {
       type_search,
       keyword,
-      category,
+      categoryId,
       filter,
     }:{
       type_search:string,
       keyword:string,
-      category:number,
-      filter: Filter | null,
+      categoryId:number,
+      filter: Filter | null | undefined,
     } = req.body;
     if (
       (typeof type_search !== 'string')
       || (typeof keyword !== 'string')
-      || (typeof category !== 'number')
+      || (typeof categoryId !== 'number')
     ) {
-        return res.status(400).send('Wrong type of data or missing fields');
+      return res.status(400).send('Wrong type of data or missing fields');
     }
     try {
-      switch(type_search){
-        case "category":
-          if(filter !== null){
-            if(filter.price_max !== null && filter.price_min !== null){
-              const products = await routesContext.db.getSearchProductCategoryFilter(category, filter.price_min,filter.price_max);
-              res.status(200).send(products);  
-            } else if(filter.price_min !== null && filter.price_max == null){
-              const products = await routesContext.db.getSearchProductCategoryFilterMin(category, filter.price_min);
+      switch (type_search) {
+        case 'category':
+          if (filter && filter !== null) {
+            if (filter && filter?.price_max !== null && filter?.price_min !== null) {
+              const products = await routesContext
+                .db.getSearchProductCategoryFilter(
+                  categoryId,
+                  filter?.price_min,
+                  filter?.price_max,
+                );
               res.status(200).send(products);
-            } else if (filter.price_max !== null && filter.price_min == null){
-              const products = await routesContext.db.getSearchProductCategoryFilterMax(category, filter.price_max);
+            } else if (filter.price_min !== null && filter.price_max == null) {
+              const products = await routesContext
+                .db.getSearchProductCategoryFilterMin(categoryId, filter.price_min);
+              res.status(200).send(products);
+            } else if (filter && filter.price_max !== null && filter.price_min == null) {
+              const products = await routesContext
+                .db.getSearchProductCategoryFilterMax(categoryId, filter.price_max);
               res.status(200).send(products);
             }
           } else {
-            const products = await routesContext.db.getSearchProductCategory(category);
+            const products = await routesContext.db.getSearchProductCategory(categoryId);
             res.status(200).send(products);
           }
           break;
-        case "keyword":
-          if(filter !== null){
-            if(filter.price_max !== null && filter.price_min !== null){
-              const products = await routesContext.db.getSearchProductKeywordFilter(keyword, filter.price_min,filter.price_max);
-            res.status(200).send(products);
-            } else if(filter.price_min !== null && filter.price_max === null){
-              const products = await routesContext.db.getSearchProductKeywordFilterMin(category, filter.price_min);
+        case 'keyword':
+          if (filter && filter !== null) {
+            if (filter?.price_max !== null && filter?.price_min !== null) {
+              const products = await routesContext
+                .db.getSearchProductKeywordFilter(keyword, filter?.price_min, filter?.price_max);
               res.status(200).send(products);
-            } else if (filter.price_max !== null && filter.price_min === null){
-              const products = await routesContext.db.getSearchProductKeywordFilterMax(category, filter.price_max);
+            } else if (filter.price_min !== null && filter.price_max === null) {
+              const products = await routesContext
+                .db.getSearchProductKeywordFilterMin(categoryId, filter.price_min);
+              res.status(200).send(products);
+            } else if (filter.price_max !== null && filter.price_min === null) {
+              const products = await routesContext
+                .db.getSearchProductKeywordFilterMax(categoryId, filter.price_max);
               res.status(200).send(products);
             }
-            
-          }else{
+          } else {
             const products = await routesContext.db.getSearchProductKeyword(keyword);
             res.status(200).send(products);
           }
           break;
+        default:
+          res.status(400).send('No access');
       }
-      res.status(400).send('No access');
     } catch (e) {
-    res.status(500).send('Something went wrong');
+      res.status(500).send('Something went wrong');
     }
+    return res.status(200).send('This is unrechable');
   });
-
 };
