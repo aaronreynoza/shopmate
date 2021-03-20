26443d8e419a4196a7fe7e09e3da12f159a1963b
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2021-03-20 16:01:18 -0600

    fix linting

--- backend/src/utils/jwtValidator.ts
+++ backend/src/utils/jwtValidator.ts
@@ -1,32 +1,32 @@
-import { NextFunction, Request, Response } from "express";
-import config from '../config';
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-function verifyToken(req:Request, res:Response, next:NextFunction){
-  const bearerHeader =  req.headers['authorization'];
-  if(typeof bearerHeader !== 'undefined'){
-      const bearerToken = bearerHeader.split(" ")[1];
-      const payload:any = jwt.verify(bearerToken, config.jwtSeed, (error, authData) => {
-        if(error){ 
-          return res.status(400).json(
-            {
-              "statos":400,
-              "data":error,
-              "message":"Invalid Token"
-            }
-          )
-        }else{
-          return authData
-        }
-      });
-      console.log(payload);
-      next();
+import config from '../config';
+
+function verifyToken(req:Request, res:Response, next:NextFunction) {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1];
+    const payload:any = jwt.verify(bearerToken, config.jwtSeed, (error, authData) => {
+      if (error) {
+        return res.status(400).json(
+          {
+            statos: 400,
+            data: error,
+            message: 'Invalid Token',
+          },
+        );
+      }
+      return authData;
+    });
+    console.log(payload);
+    next();
   } else {
     res.status(500).json(
       {
-        "statos":500,
-        "data":[],
-        "message":"Invalid Token"
-      }
+        statos: 500,
+        data: [],
+        message: 'Invalid Token',
+      },
     );
   }
 }
--- backend/src/utils/jwtValidatorAdmin.ts
+++ backend/src/utils/jwtValidatorAdmin.ts
@@ -1,42 +1,41 @@
-import { NextFunction, Request, Response } from "express";
-import config from '../config';
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-function verifyTokenAdmin(req:Request, res:Response, next:NextFunction){
-  const bearerHeader =  req.headers['authorization'];
-  if(typeof bearerHeader !== 'undefined'){
-      const bearerToken = bearerHeader.split(" ")[1];
-      const payload:any = jwt.verify(bearerToken, config.jwtSeed, (error, authData) => {
-        if(error){ 
-          return res.status(400).json(
-            {
-              "statos":400,
-              "data":error,
-              "message":"Invalid Token"
-            }
-          )
-        }else{
-          return authData
-        }
-      });
-      console.log(payload.user1[0].fk_id_tipo)
-      if(payload.user1[0].fk_id_tipo !== 1){
+import config from '../config';
+
+function verifyTokenAdmin(req:Request, res:Response, next:NextFunction) {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1];
+    const payload:any = jwt.verify(bearerToken, config.jwtSeed, (error, authData) => {
+      if (error) {
         return res.status(400).json(
           {
-            "statos":400,
-            "data":[],
-            "message":"Invalid User for this endpoint"
-          }
-        )
+            statos: 400,
+            data: error,
+            message: 'Invalid Token',
+          },
+        );
       }
-      next();
-  }else{
-    res.status(500).json(
-      {
-        "statos":500,
-        "data":[],
-        "message":"Invalid Token"
-      }
-    );
+      return authData;
+    });
+    console.log(payload.user1[0].fk_id_tipo);
+    if (payload.user1[0].fk_id_tipo !== 1) {
+      return res.status(400).json(
+        {
+          statos: 400,
+          data: [],
+          message: 'Invalid User for this endpoint',
+        },
+      );
+    }
+    next();
   }
+  return res.status(500).json(
+    {
+      statos: 500,
+      data: [],
+      message: 'Invalid Token',
+    },
+  );
 }
 export default verifyTokenAdmin;
