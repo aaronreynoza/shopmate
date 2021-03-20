9141dca3d1d11afac452e7d92b0bda2c695560e9
JesusZelayaG <49107545+JesusZelayaG@users.noreply.github.com>
2021-03-20 16:08:15 -0600

    Add function that validates the token (#44)
    * Add function that validates the token
    
    * Update backend/src/utils/jwtValidator.ts
    
    * fix linting
    
    Co-authored-by: Aarón Reynoza <aaron.alejandro.reynoza@gmail.com>

--- backend/src/utils/jwtValidatorAdmin.ts
+++ backend/src/utils/jwtValidatorAdmin.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../config';
 
-function verifyToken(req:Request, res:Response, next:NextFunction) {
+function verifyTokenAdmin(req:Request, res:Response, next:NextFunction) {
   const bearerHeader = req.headers.authorization;
   if (typeof bearerHeader !== 'undefined') {
     const bearerToken = bearerHeader.split(' ')[1];
@@ -18,16 +18,24 @@ function verifyToken(req:Request, res:Response, next:NextFunction) {
       }
       return authData;
     });
-    console.log(payload);
+    console.log(payload.user1[0].fk_id_tipo);
+    if (payload.user1[0].fk_id_tipo !== 1) {
+      return res.status(400).json(
+        {
+          statos: 400,
+          data: [],
+          message: 'Invalid User for this endpoint',
+        },
+      );
+    }
     next();
-  } else {
-    res.status(500).json(
-      {
-        statos: 500,
-        data: [],
-        message: 'Invalid Token',
-      },
-    );
   }
+  return res.status(500).json(
+    {
+      statos: 500,
+      data: [],
+      message: 'Invalid Token',
+    },
+  );
 }
-export default verifyToken;
+export default verifyTokenAdmin;
