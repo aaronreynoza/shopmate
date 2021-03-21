9c7534dea72073e16b49c840e27971456110c411
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2021-03-20 20:37:38 -0600

    add unit tests

--- backend/test/utils.test.ts
+++ backend/test/utils.test.ts
@@ -1,5 +1,35 @@
-describe('test', () => {
+import { renameCategory, renameInventory } from '../src/utils/dataChanges';
+
+describe('Utils', () => {
   test('Should return 1 + 1 = 2', () => {
     expect(1 + 1).toEqual(2);
+  });
+
+  test('Should correctly rename a category', () => {
+    expect(renameCategory({
+      id_categoria: 1,
+      nombre_categoria: 'test',
+      icon: 'icon',
+      estado: 1,
+      descripcion_categoria: 'desc',
+    })).toEqual({
+      categoryId: 1,
+      name: 'test',
+      icon: 'icon',
+      active: 1,
+      description: 'desc',
+    });
+  });
+
+  test('Should properly rename the inventory', () => {
+    expect(renameInventory({
+      cantidad: 5,
+      fk_id_producto: 1,
+      fk_id_sucursal: 3,
+    })).toEqual({
+      quantity: 5,
+      idProduct: 1,
+      idOffice: 3,
+    });
   });
 });
